748ed6b048674c04bee307c9c75fff29c9289667
Er. Amit Kumar <121847237+amit7974@users.noreply.github.com>
2026-07-20 20:55:24 +0530

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudFullstack.Model
+{
+    public class Product
+    {
+        public int productId { get; set; }
+        //createvalidation for productName
+        [Required(ErrorMessage = "Product Name is required")]
+        public string? productName { get; set; }
+
+        public double productPrice { get; set; }
+
+        public string? productDescription { get; set; } = null;
+        public string productCategory { get; set; }
+        public bool IsExpire { get; set; }
+
+        public DateTime DateTime {  get; set; }
+        
+           
+        }
+    }
