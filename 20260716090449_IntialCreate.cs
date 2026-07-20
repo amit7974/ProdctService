a782bddd334fb13c7ed3e18c2178b43441719936
Er. Amit Kumar <121847237+amit7974@users.noreply.github.com>
2026-07-20 20:54:48 +0530

    Add files via upload

---
+++ 20260716090449_IntialCreate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CrudFullstack.Migrations
+{
+    /// <inheritdoc />
+    public partial class IntialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    productId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    productName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    productPrice = table.Column<double>(type: "float", nullable: false),
+                    productDescription = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    productCategory = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsExpire = table.Column<bool>(type: "bit", nullable: false),
+                    DateTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.productId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
