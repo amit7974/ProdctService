0dfa94c9e7c84fc4eeeb39901bf8b178c774d574
Er. Amit Kumar <121847237+amit7974@users.noreply.github.com>
2026-07-20 20:54:01 +0530

    Add files via upload

---
+++ AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using CrudFullstack.Model;
+using Microsoft.EntityFrameworkCore;
+using CrudFullstack.Model;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.Identity.Client;
+
+namespace CrudFullstack.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            //Register models with the DbContext
+        }
+        public DbSet<Product> Products {  get; set; }
+    }
+
+        }
+    
+
